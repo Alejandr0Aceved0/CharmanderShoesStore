39bfca86e4d62196ce1e5e7c84aeaf92bf47b211
Alejandro <alejossena12@gmail.com>
2023-08-07 23:02:56 -0500

    feature: banner like adds style added

--- app/src/main/java/com/example/tiendadezapatos/ui/productos/ProductosFragment.java
+++ app/src/main/java/com/example/tiendadezapatos/ui/productos/ProductosFragment.java
@@ -2,6 +2,7 @@ package com.example.tiendadezapatos.ui.productos;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,7 +10,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ActionTypes;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.interfaces.TouchListener;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.tiendadezapatos.R;
 import com.example.tiendadezapatos.ui.productos.adapter.ProductoAdapter;
 import com.example.tiendadezapatos.ui.productos.model.ProductoModel;
@@ -19,14 +26,33 @@ import java.util.List;
 
 public class ProductosFragment extends Fragment {
 
+
+    ImageSlider imageSlider;
     RecyclerView recyclerViewProducto;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_productos, container, false);
 
-        recyclerViewProducto = view.findViewById(R.id.recyclerViewProductos);
+        recyclerViewProducto = view.findViewById(R.id.recycler_view_productos);
+        imageSlider = view.findViewById(R.id.image_slider_banner);
+
+
+        List<SlideModel> imageSliders = new LinkedList<>();
+        imageSliders.add(new SlideModel("https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/8463e545-701f-496d-a794-3bf38910d604/calzado-de-running-en-carretera-infinityrn-4-ftm0ff.png", ScaleTypes.CENTER_CROP));
+        imageSliders.add(new SlideModel(R.drawable.banner_2,  ScaleTypes.FIT));
+        imageSliders.add(new SlideModel(R.drawable.banner_3, "banner 3", ScaleTypes.FIT));
+        imageSliders.add(new SlideModel("R.drawable.banner_3",  ScaleTypes.FIT));
+        imageSlider.setImageList(imageSliders);
+
+        imageSlider.setTouchListener(new TouchListener() {
+            @Override
+            public void onTouched(@NonNull ActionTypes actionTypes, int i) {
+                Toast.makeText(getContext(), "CLICKEADO", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         List<ProductoModel> productos = new LinkedList<>();
 
@@ -43,5 +69,6 @@ public class ProductosFragment extends Fragment {
         recyclerViewProducto.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerViewProducto.setAdapter(adapter);
         return view;
+
     }
 }
