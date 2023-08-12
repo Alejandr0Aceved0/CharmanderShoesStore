40daaf0e8b0d73182f6a09c29dda43c2e5574c5e
Alejandro Acevedo <cacevedo@skgtecnologia.com>
2023-08-11 19:26:54 -0500

    feature: Authentication wit sharedPreferences

--- app/src/main/java/com/example/tiendadezapatos/activities/LoginActivity.java
+++ app/src/main/java/com/example/tiendadezapatos/activities/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.tiendadezapatos.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,11 @@ import com.example.tiendadezapatos.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private String ARCHIVO_SHARED_PREFERENCES = "DATOS_USUARIO";
+
+    private String KEY_CORREO = "correo";
+    private String KEY_PASS = "pass";
+    private String KEY_ROL = "rol";
 
     EditText edCorreo, edContrasena;
     TextView txtOlvidoPass;
@@ -23,6 +29,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        iniciarSesionSharedPreferences();
 
         edCorreo = findViewById(R.id.txtCorreo);
         edContrasena = findViewById(R.id.txtPass);
@@ -65,12 +73,44 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void iniciarSesion() {
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
+
+        String correo = edCorreo.getText().toString();
+        String pass = edContrasena.getText().toString();
+
+        if (pass.equals("hola")){
+
+            //SE GUARDAN DATOS DE LOGIN EN ARCHIVOP XML
+            SharedPreferences shared = getSharedPreferences(ARCHIVO_SHARED_PREFERENCES, MODE_PRIVATE);
+            SharedPreferences.Editor editor = shared.edit();
+            editor.putString(KEY_CORREO, correo);
+            editor.putString(KEY_PASS, pass);
+            editor.putString(KEY_ROL, "rol");
+            editor.apply(); //.apply() guarda los datos seteados por putString
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }else{
+            Toast.makeText(getBaseContext(), "PASS INCORRECTO", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void registrarUsuario() {
         Intent intent = new Intent(LoginActivity.this, RegistroActivity.class);
         startActivity(intent);
+    }
+
+    private void iniciarSesionSharedPreferences(){
+        SharedPreferences sharedPreferences = getSharedPreferences(ARCHIVO_SHARED_PREFERENCES, MODE_PRIVATE);
+
+        String correo = sharedPreferences.getString(KEY_CORREO, null);
+        String pass = sharedPreferences.getString(KEY_PASS, null);
+        String rol = sharedPreferences.getString(KEY_ROL, null);
+
+        if (correo != null && pass != null && rol != null){
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+
     }
 }
